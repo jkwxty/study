374657293b7ac28f299197eb80605ce9434fcdf1
jiangkangwei <jiangkangwei@talkweb.com.cn>
2022-08-22 17:48:56 +0800

    openFeign 配置设置

--- order-openfeign/src/main/java/com/it/alibaba/config/FeignConfig.java
+++ order-openfeign/src/main/java/com/it/alibaba/config/FeignConfig.java
@@ -1,6 +1,9 @@
 package com.it.alibaba.config;
 
+import com.it.alibaba.customer.MyCustomerFeignInterceptor;
 import feign.Logger;
+import feign.Request;
+import feign.RequestInterceptor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -13,8 +16,25 @@ import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class FeignConfig {
+
+  /**
+   * 设置日志优先级，还需要再配置文件中修改spring boot 默认的日志有限级别为
+   * @return
+   */
   @Bean
   public Logger.Level feignLoggerLevel(){
     return Logger.Level.FULL;
+  }
+
+  @Bean
+  public Request.Options options(){
+    //1 连接超时时间，2 请求处理超时时间
+    return new Request.Options(5000,10000);
+  }
+
+  @Bean
+  public MyCustomerFeignInterceptor requestInterceptor(){
+    //自定义的feign拦截器
+    return new MyCustomerFeignInterceptor();
   }
 }
